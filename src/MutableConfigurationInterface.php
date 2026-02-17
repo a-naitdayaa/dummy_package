dad4f2248705b9502ff174b1ab61a0d50349e7d9
a-naitdayaa <a.naitdayaa@void.fr>
2026-02-17 10:40:37 +0000

    Initial commit

---
+++ src/MutableConfigurationInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the league/config package.
+ *
+ * (c) Colin O'Dell <colinodell@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace League\Config;
+
+/**
+ * Interface for setting/merging user-defined configuration values into the configuration object
+ */
+interface MutableConfigurationInterface
+{
+    /**
+     * @param mixed $value
+     *
+     * @throws UnknownOptionException if $key contains a nested path wich does'nt point to an array value
+     */
+    public function set(string $key, $value): void;
+
+    /**
+     * @param array<string, mixed> $config
+     */
+    public function merge(array $config = []): void;
+}
